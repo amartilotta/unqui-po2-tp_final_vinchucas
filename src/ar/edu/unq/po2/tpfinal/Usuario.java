4e27719cb71a3a1045e73b4b0776a7ccd1c49732
Agustin Martilotta <agustin.martilotta@hotmail.com>
2023-06-15 02:54:09 -0300

    Clase usuario y creacion de carpeta test

--- src/ar/edu/unq/po2/tpfinal/Usuario.java
+++ src/ar/edu/unq/po2/tpfinal/Usuario.java
@@ -1,4 +1,98 @@
+package ar.edu.unq.po2.tpfinal;
+
+import java.util.ArrayList;
+import java.time.LocalDate;
 
 public class Usuario {
+	
+	
+	
+	//ATRIBUTOS
+	private Integer ID;
+	private String nombre;
+	private Nivel nivel;
+	private ArrayList<Muestra> listaDeMuestrasPosteadas = new ArrayList<Muestra>();
+	private ArrayList<Opinion> listaDeOpiniones = new ArrayList<Opinion>();
+	
+	
+	
+	//CONSTRUCTOR
+	public Usuario (String nombre, Integer id) {
+		this.nombre = nombre;
+		this.ID = id;
+		this.setNivel(new NivelBasico());
+		
+	}
+	
+	
+	
+	//SETTERS
+	public void setNivel(Nivel nivel) {
+		this.nivel = nivel;
+		this.nivel.setUsuario(this);
+	}
+	
+	
+	
+	//GETTERS
+	public Nivel getNivel() {
+		return this.nivel;
+	}
+	
+	public ArrayList<Muestra> getListaMuestras(){
+		return this.listaDeMuestrasPosteadas;
+	}
+	public ArrayList<Opinion> getListaOpiniones(){
+		return this.listaDeOpiniones;
+	}
+	
+	public String getNombre() {
+		return this.nombre;
+	}
 
+	public Integer getID() {
+		return this.ID;
+	}
+	
+	
+	
+	//METODOS
+	public void cambiarNivel() {
+		this.nivel.actualizarNivelUsuario(this);
+	} 
+	
+	public void opinar(Muestra muestra, Comentario comentario) {
+		this.nivel.opinar(muestra,comentario);
+	}
+
+	public void añadirMuestraEnviada(Muestra muestra) {
+		this.listaDeMuestrasPosteadas.add(muestra);
+		
+	}
+	public void añadirOpinionEnviada(Opinion opinion) {
+		this.listaDeOpiniones.add(opinion);
+		
+	}
+
+	public int cantidadMuestrasEnviadasEnElUltimoMes() {
+		int contador = 0;
+		LocalDate fechaActual = LocalDate.now();
+		for ( Muestra muestra : listaDeMuestrasPosteadas) {
+			if (muestra.fechaCreacion().isAfter(fechaActual.minusDays(30))  ) {
+				contador ++;
+			}
+		}
+		return contador;
+	}
+	
+	public int cantidadOpinionesEnviadasEnElUltimoMes() {
+		int contador = 0;
+		LocalDate fechaActual = LocalDate.now();
+		for ( Opinion opinion : this.listaDeOpiniones) {
+			if (opinion.getFechaDeOpinion().isAfter(fechaActual.minusDays(30))) {
+				contador ++;
+			}
+		}
+		return contador;
+	}
 }
---
+++ test/ar/edu/unq/po2/tpFinal/UsuarioTestCase.java
@@ -0,0 +1,115 @@
+package ar.edu.unq.po2.tpFinal;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import static org.mockito.Mockito.*;
+
+import java.time.LocalDate;
+
+
+import org.junit.jupiter.api.BeforeEach;
+
+public class UsuarioTestCase {
+	//aca van las declaraciones de las variables
+	private Usuario usuarioTest;
+	private Muestra muestraTest;
+	private Opinion opinionTest;
+	private SistemaVinchuca sistema;
+	private Nivel nivelBasicoTest;
+	private Nivel nivelExpertoTest;
+	//private Comentario comentario;
+	@BeforeEach
+	public void setUp() {
+		//aca le doy valor a todo lo necesario
+		usuarioTest = new Usuario("Pepe", 1);
+		muestraTest = mock(Muestra.class); 
+		opinionTest = mock(Opinion.class);
+		sistema = mock(SistemaVinchuca.class);
+		nivelBasicoTest = mock(NivelBasico.class);
+		nivelExpertoTest = mock(NivelExperto.class);
+		
+		
+	}
+	
+//	@Test
+//	void cuandoUnUsarioOpinaDelegaASuNivelLaAccionDeOpinar() {
+//		Comentario comentario = Comentario.ChicheFoliada;
+//		usuarioTest.opinar(muestraTest, comentario);
+//		verify(usuarioTest).methodCall
+//	} 
+	
+
+	@Test
+	void cuandoUnUsuarioAñadeUnaMuestraLalistaDeMuestrasIncrementaEn1() {
+		usuarioTest.añadirMuestraEnviada(muestraTest);
+		assertEquals(1,usuarioTest.getListaMuestras().size()); 
+	} 
+	
+	@Test
+	void cuandoUnUsuarioAñadeUnaOpinionLalistaDeOpinonesIncrementaEn1() {
+		usuarioTest.añadirOpinionEnviada(opinionTest);
+		assertEquals(1,usuarioTest.getListaOpiniones().size()); 
+	} 
+	
+	@Test
+	void cuandoUnUsuarioLeConsultoSuNivelMeLoRetorna() {
+		usuarioTest.setNivel(nivelBasicoTest);
+		assertEquals(nivelBasicoTest,usuarioTest.getNivel()); 
+	} 
+	
+	@Test
+	void cuandoUnUsuarioLeSeteoNivelExpertoElUsuarioPasaANivelExperto() {
+		usuarioTest.setNivel(nivelExpertoTest);
+		assertEquals(nivelExpertoTest,usuarioTest.getNivel()); 
+	} 
+	
+	@Test
+	void cuandoUnUsuarioLeConsultoSuNombreMeLoDevuelve() {
+		String nombreUsuarioEsperado = "Pepe";
+		assertEquals(nombreUsuarioEsperado,usuarioTest.getNombre()); 
+	} 
+	
+	
+	@Test
+	void cuandoUnUsuarioLeConsultoSuIDMeLoDevuelve() {
+		Integer idEsperado = 1;
+		assertEquals(idEsperado,usuarioTest.getID()); 
+	} 
+	
+	@Test
+	void cuandoUnUsuarioPosteo0MuestrasEnElUltimoMesYLeConsultanCuantasMuestrasPosteoDevuelve1() {
+		usuarioTest.añadirMuestraEnviada(muestraTest);
+		when(muestraTest.fechaCreacion()).thenReturn(LocalDate.now().minusDays(40));
+		
+		assertEquals(0,usuarioTest.cantidadMuestrasEnviadasEnElUltimoMes()); 
+	} 
+	@Test
+	void cuandoUnUsuarioPosteo1MuestrasEnElUltimoMesYLeConsultanCuantasMuestrasPosteoDevuelve1() {
+		usuarioTest.añadirMuestraEnviada(muestraTest);
+		when(muestraTest.fechaCreacion()).thenReturn(LocalDate.now().minusDays(10));
+		
+		assertEquals(1,usuarioTest.cantidadMuestrasEnviadasEnElUltimoMes()); 
+	} 
+	
+	@Test
+	void cuandoUnUsuarioOpino0MuestrasEnElUltimoMesYLeConsultanCuantasOpinionesEnvioDevuelve1() {
+		usuarioTest.añadirOpinionEnviada(opinionTest);
+		when(opinionTest.getFechaDeOpinion()).thenReturn(LocalDate.now().minusDays(40));
+		
+		assertEquals(0,usuarioTest.cantidadOpinionesEnviadasEnElUltimoMes()); 
+	} 
+	@Test
+	void cuandoUnUsuarioPosteo1MuestraEnElUltimoMesYLeConsultanCuantasOpinionesEnvioDevuelve1() {
+		usuarioTest.añadirOpinionEnviada(opinionTest);
+		when(opinionTest.getFechaDeOpinion()).thenReturn(LocalDate.now().minusDays(10));
+		
+		assertEquals(1,usuarioTest.cantidadOpinionesEnviadasEnElUltimoMes()); 
+	} 
+	
+	
+
+
+}
+
